ba72668545c40e39d7772242bfcbb4fd732b9e1a
gauravv0579 <gauravkumar0579@gmail.com>
2023-03-22 11:32:22 +0530

    pulling product info and cleaning the code

--- Django Amazon Search App/amazon_search/views.py
+++ Django Amazon Search App/amazon_search/views.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from django.shortcuts import render
+from autoscraper import AutoScraper
 
 def search_keyword(request):
     if request.method == 'GET':
@@ -13,18 +14,12 @@ def search_keyword(request):
                            'Chrome/44.0.2403.157 Safari/537.36'),
             'Accept-Language': 'en-US, en;q=0.5'
         }
-        #url = f'https://www.amazon.in/s?k={keyword}&crid=UECC134EG63U&sprefix={keyword}%2Caps%2C195&ref=nb_sb_noss_1'
-        #url = f'https://www.amazon.com/s?k={keyword}'
-        url = "https://www.amazon.com/iPhone-13-128GB-Pink-Unlocked/dp/B0BGQS8YV4/ref=sr_1_2?keywords=iphone&qid=1679393336&sr=8-2"
+        url = f'https://www.amazon.com/s?k={keyword}'
         response = requests.get(url,headers=HEADERS)
-        soup = BeautifulSoup(response.text)
-        #soup = soup.find('span', {'id': "productTitle"}).text.strip()
-        #soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, 'html.parser')
         products = []
         for product in soup.find_all('div', {'class': 's-result-item'}):
             name = product.find('h2').text.strip()
             price = product.find('span', {'class': 'a-price-whole'}).text.strip()
-            image_url = product.find('img')['src']
-            products.append({'name': name, 'price': price, 'image_url': image_url})
-        products = ["Samsung","Apple"]
+            products.append({'name': name, 'price': price})
         return render(request, 'index.html', {'keyword': keyword, 'products': products})
